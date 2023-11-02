0790196d58ef1584b1d10f2856ef0529fd26ee71
Matthew <mcho9434@usc.edu>
2023-11-02 01:31:55 -0700

    move into try

--- backend/controller/college.controller.js
+++ backend/controller/college.controller.js
@@ -1,32 +1,39 @@
 require("dotenv").config();
 const db = require("../db");
 
-class collegeController {
-  async allColleges() {
-    try {
-      const result = await db.query("SELECT * FROM colleges;", []);
-      return result.rows;
-    } catch (error) {
-      return error;
-    }
-  }
 
-  async editCollege(newFields, college_id) {
-    try {
-      const query =
-        "UPDATE colleges SET " +
-        Object.keys(newFields)
-          .map((column) => column + "='" + newFields[column] + "'")
-          .join(", ") +
-        " WHERE college_id=" +
-        college_id +
-        " RETURNING *;";
-      const result = await db.query(query, []);
-      return result.rows;
-    } catch (error) {
-      return error;
+class collegeController {
+
+    async allColleges() {
+        try {
+            const result = await db.query(
+                "SELECT * FROM colleges;",
+                []
+            );
+            return result.rows;
+        } catch (error) {
+            return error;
+        }
     }
-  }
+
+    async editCollege(newFields, college_id) {
+        
+        try {
+            const query =
+                "UPDATE colleges SET " +
+                Object.keys(newFields)
+                .map((column) => column + "='" + newFields[column] + "'")
+                .join(", ") +
+                " WHERE college_id=" +
+                college_id +
+                " RETURNING *;";
+            const result = await db.query(query, []);
+            return result.rows;
+        } catch (error) {
+            return error;
+        }
+    }
+
 }
 
 module.exports = new collegeController();
