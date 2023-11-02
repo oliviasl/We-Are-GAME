4e2eadd21b1b9ae1b17d40b900a97db2ae227639
Matthew <mcho9434@usc.edu>
2023-11-02 01:23:57 -0700

    add route and controller.

--- backend/controller/college.controller.js
+++ backend/controller/college.controller.js
@@ -16,6 +16,22 @@ class collegeController {
         }
     }
 
+    async editCollege(newFields, college_id) {
+        const query =
+            "UPDATE colleges SET " +
+            Object.keys(newFields)
+            .map((column) => column + "='" + newFields[column] + "'")
+            .join(", ") +
+            " WHERE college_id=" +
+            college_id +
+            " RETURNING *;";
+        try {
+            const result = await db.query(query, []);
+            return result.rows;
+        } catch (error) {
+            return error;
+        }
+    }
 
 }
 
