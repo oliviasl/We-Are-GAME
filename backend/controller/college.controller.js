0c52e7207a9700b6c031003add63b4bc0b980f84
Cole Gawin <colegawin@gmail.com>
2023-11-08 12:56:16 -0800

    feat: add `/api/collegesByGPA` route

--- backend/controller/college.controller.js
+++ backend/controller/college.controller.js
@@ -17,6 +17,14 @@ class collegeController {
     }
 
 
+    async collegesByGPA(gpa) {
+        const result = await db.query(
+            "SELECT * FROM colleges WHERE $1 BETWEEN min_gpa AND max_gpa;",
+            [gpa]
+        );
+        return result.rows;
+    }
+
 }
 
 module.exports = new collegeController();
--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,20 @@ app.get("/api/allColleges", (req, res) => {
 // returns data of colleges with non null diversity_resources_web_addr
 
 
-// collegeByGPA
+// collegesByGPA
 // returns data of colleges that include GPA within their min/max
-
+app.get("/api/collegesByGPA", (req, res) => {
+  const { gpa } = req.body;
+  collegeController
+    .collegesByGPA(gpa)
+    .then((data) =>
+      res.status(200).json(data)
+    )
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ error });
+    });
+});
 
 // collegeBySATRead
 // returns data of colleges that include SAT Reading/Writing score within their min/max
