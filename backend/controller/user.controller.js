48c86350e0adf38b81396377e2cf6557a80bcf88
Matthew <mcho9434@usc.edu>
2023-11-30 22:39:00 -0800

    add blocks for mandaotry fields

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -14,46 +14,49 @@ class userController {
 
     // createUser
     async createUser(userData) {
-        console.log(userData);
-        if(!(userData.user_email && userData.user_password && userData.user_firstname && userData.user_lastname)){
-            console.log("Missing mandatory field");
-            return false;
-        }   
-
-        const email = userData.user_email;
-
-        const query = `SELECT COUNT(*) AS count FROM master_users WHERE user_email = '${email}'`;
-
-        const emailMatch = await db.query(query, []);
-
-        if(emailMatch.rows[0].count!=0){
-            //email already exists
-            console.log("Matching email already exists");
-            return false;
+        if (
+          !(
+            userData.user_email &&
+            userData.user_password &&
+            userData.user_firstname &&
+            userData.user_lastname
+          )
+        ) {
+          console.log("Missing mandatory field");
+          return false;
         }
-
+    
+        const email = userData.user_email;
+        const query = `SELECT COUNT(*) AS count FROM master_users WHERE user_email = '${email}'`;
+    
+        const emailMatch = await db.query(query, []);
+    
+        if (emailMatch.rows[0].count != 0) {
+          //email already exists
+          console.log("Matching email already exists");
+          return false;
+        }
+    
         //taken from
         const insertKeys = Object.keys(userData);
         const insertValues = Object.values(userData);
-      
+    
         // create placeholder values ($1, $2, etc.) for each value to be inserted
         const placeholders = insertKeys
-            .map((_, index) => `$${index + 1}`)
-            .join(", ");
-
+          .map((_, index) => `$${index + 1}`)
+          .join(", ");
+    
         const insertQuery = `
-            INSERT INTO master_users (${insertKeys.join(", ")})
-            VALUES (${placeholders});
-        `;
+                INSERT INTO master_users (${insertKeys.join(", ")})
+                VALUES (${placeholders});
+            `;
         try {
-            await db.query(insertQuery, insertValues);
-
+          await db.query(insertQuery, insertValues);
         } catch (error) {
-            return error;
+          return error;
         }
-
+    
         return true;
-       
     }
     // editUser
 
