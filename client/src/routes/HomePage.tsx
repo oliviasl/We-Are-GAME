5d9b73161f5daff5ab7bf0102c9a53fdf2762e6c
Greydon O'Keefe <gokeefe@usc.edu>
2024-01-24 00:07:23 -0800

    Nearly Complete
    Needs...
    1. Responsive underline border on Login vs Sign Up buttons
    2. Margin below "Request Authorization" on sign up form
    
    I tried to use flex justify-center but had difficulty so I used m-auto instead :( I made state vars for all the fields and added onChange to the inputs so that should be working though

--- client/src/routes/HomePage.tsx
+++ client/src/routes/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, ElementType } from "react";
 import { Divide, Home } from "lucide-react";
 import HomePageLinks from "../layouts/HomePageLinks";
+import { setegid } from "process";
 
 const tempRouteInfo = [
     {
@@ -27,7 +28,12 @@ const tempRouteInfo = [
 
 const HomePage = () => {
     const [tabs, setTabs] = useState("Login");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
 
     // useEffect(() => {
     //     fetch(`/api/allColleges`)
@@ -44,74 +50,65 @@ const HomePage = () => {
     //         <HomePageLinks RouteInfo={tempRouteInfo}/>
     //     </div>
     // )
-    return (
-        //              NOTES
-        // how to center div in middle of screen?
-        // not sure why width control isnt working
-        // assuming I need to useState() with the login vs signup button and control the underline through a bool?
-        
-        <div className="flex justify-center m-3">
-            <div className="w-[612px] border-2 border-black rounded">
-                <div className="flex justify-around mt-3">
+
+    return (       
+        <div className="flex justify-center m-3"> {/* Entire View */}
+            <div className="w-[612px] border-2 border-black rounded"> {/* Bordered Panel */}
+                <div className="flex justify-around mt-3"> {/* Login vs Signup Nav */}
                     <div className="cursor-pointer" onClick={()=>{setTabs("Login")}}>Login</div>
-                    <div onClick={()=>{setTabs("SignUp")}}>Sign Up</div>
+                    <div className="cursor-pointer" onClick={()=>{setTabs("SignUp")}}>Sign Up</div>
                 </div>
                 {tabs=="Login" ? 
-                    <div className="w-[400px] m-auto mt-12">
-                        <div>
+                    <div className="w-[400px] m-auto mt-12"> {/* Login Form */}
+                        <div> {/* Username Wrapper */}
                             <div className="block text-gray-700 text-sm font-bold mb-3">Username</div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3" id="username" type="text"></input>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight mb-3" id="username" type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
                         </div>
-                        <div>
+                        <div> {/* Password Wrapper */}
                             <div className="block text-gray-700 text-sm font-bold mb-3">Password</div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight mb-5" id="password" type="password"></input>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight mb-5" id="password" type="password"  value={password} onChange={e => setPassword(e.target.value)}></input>
                         </div>
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center"> {/* Login Button Wrapper */}
                             <div className="cursor-pointer bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-12">Login</div>
                         </div>
                     </div> 
                 : 
-                    <div>
-                        <div className="flex justify-center">
-                            <div className="w-[191px] mx-2">
+                    <div className="w-[400px] m-auto mt-12"> {/* Sign Up Form */}
+                        <div className="flex space-x-auto mt-17"> {/* First / Last Name */}
+                            <div className="w-[190px]"> {/* First Name */}
                                 <div className="text-gray-700 text-sm font-bold mb-3">First</div>
-                                <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                                <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="first" type="text"  value={firstName} onChange={e => setFirstName(e.target.value)}></input>
                             </div>
-                            <div className="w-[191px] mx-2">
+                            <div className="w-[20px]"></div> {/* Spacer */}
+                            <div className="w-[190px]"> {/* Last Name */}
                                 <div className="text-gray-700 text-sm font-bold mb-3">Last</div>
-                                <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                                <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="last" type="text"  value={lastName} onChange={e => setLastName(e.target.value)}></input>
                             </div>
                         </div>
-                        <div className="w-[400px] m-auto mt-5">
+                        <div className="w-[400px] m-auto mt-5"> {/* Username Wrapper */}
                             <div className="block text-gray-700 text-sm font-bold mb-2">Username</div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"  value={username} onChange={e => setUsername(e.target.value)}></input>
                         </div>
-                        <div className="w-[400px] m-auto mt-5">
+                        <div className="w-[400px] m-auto mt-5"> {/* Password Wrapper */}
                             <div className="block text-gray-700 text-sm font-bold mb-2">Password</div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="password" type="text"  value={password} onChange={e => setPassword(e.target.value)}></input>
                         </div>
-                        <div className="w-[400px] m-auto mt-5">
+                        <div className="w-[400px] m-auto mt-5"> {/* Verify Password Wrapper */}
                             <div className="block text-gray-700 text-sm font-bold mb-2 py-12px">Verify Password</div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="verifypassword" type="text" value={verifyPassword} onChange={e => setVerifyPassword(e.target.value)}></input>
                         </div>
-                        <div className="w-[400px] m-auto mt-5">
-                            <div className="block text-gray-700 text-sm font-bold mb-2 py-12px">
-                                Email
-                            </div>
-                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="username" type="text"></input>
+                        <div className="w-[400px] m-auto mt-5"> {/* Email Wrapper */}
+                            <div className="block text-gray-700 text-sm font-bold mb-2 py-12px">Email</div>
+                            <input className="border-2 border-black rounded w-full py-2 px-3 text-gray-700 leading-tight" id="email" type="text"  value={email} onChange={e => setEmail(e.target.value)}></input>
+                        </div>
+                        <div className="w-[400px] flex items-center justify-end mt-6"> {/* Auth Wrapper */}
+                            <div className="cursor-pointer bg-gray-700 text-white font-bold py-2 px-4 rounded">Request Authorization</div>
                         </div>
                     </div>
                 }
             </div>
         </div>
     );
-
-    // can we map the repetitive structure or needs to be independent because of values?
-
-    //          SIGN UP Page
-    // return (
-    
-    // );
 
     // return (
     //     <div className="h-screen flex items-center justify-center">
