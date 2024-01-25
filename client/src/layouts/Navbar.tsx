d3d195e8a78a7289125baf35b000fbe070b7a967
Yeji Seo <faithyejiseo@gmail.com>
2024-01-25 03:52:09 -0800

    Style: changed nav items to anchor tags

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -16,19 +16,21 @@ export function Navbar({ isAdmin }: NavbarProps) {
     // added padding for testing purposes: should be removed once Nav is integrated into other components, not exact green!
     <div className="z-[20] mx-auto mt-28 mb-4 px-24 flex w-full items-center justify-between bg-grey-600 text-brand-black font-bold font-grotesk">
       <div className="flex items-center gap-5">
-        <Logo />
-          <div className="text-2xl">We Are G.A.M.E.</div>
+          <a href='#'>
+            <Logo />
+          </a>
+          <div className="text-2xl"><a href='#'>We Are G.A.M.E.</a></div>
       </div>
       
       <div className="flex items-center gap-12">
-        <div className="hover:text-green-900">College Directory</div>
-        <div className="hover:text-green-900">Mentor Directory</div>
-        <div className="hover:text-green-900">Student Directory</div>
+        <div className="hover:text-green-900"><a href='#'>College Directory</a></div>
+        <div className="hover:text-green-900"><a href='#'>Mentor Directory</a></div>
+        <div className="hover:text-green-900"><a href='#'>Student Directory</a></div>
 
         {isAdmin ? (
-          <div className="hover:text-green-900">Authenticate Users</div>
+          <div className="hover:text-green-900"><a href='#'>Authenticate Users</a></div>
         ) : (
-          <div className="hover:text-green-900">Student Profile</div>
+          <div className="hover:text-green-900"><a href='#'>Student Profile</a></div>
         )}
       </div>
     </div>
