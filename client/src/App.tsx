b182a368565ebc20d2ff326e15e4f5c4df726bd6
Yeji Seo <faithyejiseo@gmail.com>
2024-01-25 21:54:10 -0800

    Feat: working route for StudentProfile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
 import AddCollege from "./routes/AddCollege";
 import HomePage from "./routes/HomePage";
+import StudentProfile from "./routes/StudentProfile";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/add-college" element={<AddCollege />} />
+          <Route path="/student-profile" element={<StudentProfile />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/routes/StudentProfile.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../layouts/Navbar";
+
+const StudentProfile = () => {
+  return (
+    <div className="">
+        <Navbar />
+        hi
+    </div>
+  )
+};
+
+export default StudentProfile;
