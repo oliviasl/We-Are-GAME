8c23bcb560d2a1e837c3a6c14913b5b49803b9c0
Matthew <mcho9434@usc.edu>
2024-01-31 09:56:03 -0800

    base fe done, needs paginated endpoint and pagination component

--- client/src/components/AuthUserRow.tsx
+++ client/src/components/AuthUserRow.tsx
@@ -2,13 +2,29 @@ import React from "react";
 
 type AuthUserRowProps = {
     name: string;
-    id: Number;
     email: string;
+    onApprove: () => void;
+    onDeny: () => void;
 };
-const AuthUserRow = ({ name, id, email }: AuthUserRowProps) => {
+const AuthUserRow = ({ name, email, onApprove, onDeny }: AuthUserRowProps) => {
     return (
-        <tr>
-            <td>{name}</td> <td>{email}</td> <td> APPROVE / DENY</td>
+        <tr className="h-12 border-b border-black">
+            <td className="pl-16">{name}</td>
+            <td className="px-8">{email}</td>
+            <td className="pr-16">
+                <span
+                    onClick={onApprove}
+                    className="my-2 px-4 py-2 rounded bg-gray-800 cursor-pointer text-white"
+                >
+                    Approve
+                </span>
+                <span
+                    onClick={onDeny}
+                    className="my-2 px-4 py-2 rounded bg-gray-800 cursor-pointer ml-2 text-white"
+                >
+                    Deny
+                </span>
+            </td>
         </tr>
     );
 };
--- client/src/routes/AuthenticateUsers.tsx
+++ client/src/routes/AuthenticateUsers.tsx
@@ -3,18 +3,80 @@ import { Navbar } from "../layouts/Navbar";
 import AuthUserRow from "../components/AuthUserRow";
 
 const AuthenticateUsers = () => {
+    const onApprove = async (id: Number) => {
+        console.log("would approve user with id", id.toString());
+    };
+    const onDeny = async (id: Number) => {
+        console.log("would deny user with id", id.toString());
+    };
+
     return (
         <div>
             <Navbar />
             <div className="mx-20 mt-16">
                 <div className="font-bold text-5xl">Authentication</div>
-                <table className="w-full px-20 table-fixed">
-                    <tr className="pxx-20">
-                        <th>Company</th>
-                        <th>Contact</th>
-                        <th>Country</th>
-                    </tr>
-                    <AuthUserRow name="bob" id={1} email="a@a.a" />
+                <table className="mt-16 w-full px-20 table-fixed">
+                    <thead>
+                        <tr className="border-y border-black">
+                            <th className="text-left py-2 pl-16">Name</th>
+                            <th className="text-left py-2 px-8">Email</th>
+                            <th className="text-left py-2 pr-16">
+                                Approve/Deny
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <AuthUserRow
+                            name="bob"
+                            email="a@a.a"
+                            onApprove={() => {
+                                onApprove(1);
+                            }}
+                            onDeny={() => {
+                                onDeny(1);
+                            }}
+                        />
+                        <AuthUserRow
+                            name="bob"
+                            email="a@a.a"
+                            onApprove={() => {
+                                onApprove(1);
+                            }}
+                            onDeny={() => {
+                                onDeny(1);
+                            }}
+                        />
+                        <AuthUserRow
+                            name="bob"
+                            email="a@a.a"
+                            onApprove={() => {
+                                onApprove(1);
+                            }}
+                            onDeny={() => {
+                                onDeny(1);
+                            }}
+                        />
+                        <AuthUserRow
+                            name="bob"
+                            email="a@a.a"
+                            onApprove={() => {
+                                onApprove(1);
+                            }}
+                            onDeny={() => {
+                                onDeny(1);
+                            }}
+                        />
+                        <AuthUserRow
+                            name="bob"
+                            email="a@a.a"
+                            onApprove={() => {
+                                onApprove(1);
+                            }}
+                            onDeny={() => {
+                                onDeny(1);
+                            }}
+                        />
+                    </tbody>
                 </table>
             </div>
         </div>
