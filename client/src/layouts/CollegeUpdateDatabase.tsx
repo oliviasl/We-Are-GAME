ea471f229f0a99a83ffca09f1dd25febfc6a7e56
Kristen Won <kjwon@usc.edu>
2024-01-31 19:59:19 -0800

    progress

--- client/src/layouts/CollegeUpdateDatabase.tsx
+++ client/src/layouts/CollegeUpdateDatabase.tsx
@@ -3,10 +3,10 @@ import { Button } from "@material-tailwind/react";
 
 const CollegeUpdateDatabase = () => {
     return (
-        <div className="pb-16 pt-[19px] pr-5 flex items-center justify-between">
+        <div className="pb-16 pt-[19px] pr-5 flex items-baseline justify-between">
             <div className="font-bold text-5xl">College Database</div>
 
-            <div><Button>Update Database</Button></div>
+            <div className="h-9 rounded"><Button color="gray">Update Database</Button></div>
         </div>
     );
 };
