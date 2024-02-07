43409648c4efc4dbfdafdcf2aa33cfa799994aa5
Cole Gawin <colegawin@gmail.com>
2024-02-07 01:40:23 -0800

    feat: send college data to backend

--- client/src/routes/AddCollege.tsx
+++ client/src/routes/AddCollege.tsx
@@ -1,7 +1,22 @@
 import CollegeFormPage from "../layouts/CollegeFormPage";
 
+import {useNavigate} from "react-router-dom"
+
 const AddCollege = () => {
-  return <CollegeFormPage/>
+  const navigate = useNavigate();
+
+  async function onSubmit(data: any) {
+    await fetch("/api/createCollege", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({collegeData: data}),
+    })
+    navigate("/")
+  }
+
+  return <CollegeFormPage onSubmit={onSubmit}/>
 }
 
 export default AddCollege;
