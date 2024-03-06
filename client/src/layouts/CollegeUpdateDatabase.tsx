6d6a963c4ac557b206148c9e551b60931da2df4e
oliviasl <oslai@usc.edu>
2024-03-06 15:18:58 -0800

    Update admin user status check

--- client/src/layouts/CollegeUpdateDatabase.tsx
+++ client/src/layouts/CollegeUpdateDatabase.tsx
@@ -12,7 +12,7 @@ const CollegeUpdateDatabase = () => {
     <div className="pb-16 pt-[19px] pr-5 flex items-center justify-between">
       <div className="font-bold text-5xl">College Database</div>
 
-      {cookies.user_status === 1 && <div className={"space-x-2"}>
+      {cookies.user_status === 3 && <div className={"space-x-2"}>
         <Button color="gray" variant={"outlined"}>Update Database</Button>
         <Button color="gray" onClick={() => navigate("/add-college")}>Add College</Button>
       </div>}
