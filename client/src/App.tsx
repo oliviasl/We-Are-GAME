48cdee5ab0a51412771a848f8ecddd5a9d5922b6
Yeji Seo <faithyejiseo@gmail.com>
2024-03-18 01:32:31 -0700

    Chore: added id to route of editStudent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import AddCollege from "./routes/AddCollege";
 import HomePage from "./routes/HomePage";
 import AuthenticateUsers from "./routes/AuthenticateUsers";
@@ -9,37 +9,39 @@ import StudentProfileRoute from "./routes/StudentProfile";
 import EditStudent from "./routes/EditStudent";
 import CollegeProfile from "./routes/CollegeProfile";
 import EditCollege from "./routes/EditCollege";
-import {ThemeProvider} from "@material-tailwind/react";
+import { ThemeProvider } from "@material-tailwind/react";
 import theme from "./util/theme";
-import {Navbar} from "./layouts/Navbar";
-import {ToastContainer} from "react-toastify";
+import { Navbar } from "./layouts/Navbar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <ThemeProvider value={theme}>
       <div>
-        <Navbar/>
+        <Navbar />
         <Router>
           <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/add-college" element={<AddCollege/>}/>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add-college" element={<AddCollege />} />
             <Route path="/edit-college">
-              <Route path=":id" element={<EditCollege/>}/>
+              <Route path=":id" element={<EditCollege />} />
             </Route>
-            <Route path="/authenticate" element={<AuthenticateUsers/>}/>
-            <Route path="/college-database" element={<CollegeDatabase/>}/>
+            <Route path="/authenticate" element={<AuthenticateUsers />} />
+            <Route path="/college-database" element={<CollegeDatabase />} />
             <Route path="/student-profile">
-              <Route path=":id" element={<StudentProfileRoute/>}/>
+              <Route path=":id" element={<StudentProfileRoute />} />
             </Route>
-            <Route path="/edit-student" element={<EditStudent/>} />
+            <Route path="/edit-student">
+              <Route path=":id" element={<EditStudent />} />
+            </Route>
             <Route path="/college-profile">
-              <Route path=":id" element={<CollegeProfile/>}/>
+              <Route path=":id" element={<CollegeProfile />} />
             </Route>
-            <Route path="/student-database" element={<StudentDatabase/>}/>
+            <Route path="/student-database" element={<StudentDatabase />} />
           </Routes>
         </Router>
       </div>
-      <ToastContainer hideProgressBar={true} position={"bottom-right"}/>
+      <ToastContainer hideProgressBar={true} position={"bottom-right"} />
     </ThemeProvider>
   );
 }
