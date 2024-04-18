5ada1b1f693c1b29bce4f28e8670644652221669
oliviasl <oslai@usc.edu>
2024-04-18 13:03:08 -0700

    Remove mentor socials

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -132,8 +132,6 @@ CREATE TABLE IF NOT EXISTS mentors
     mentor_firstname VARCHAR,
     mentor_lastname VARCHAR,
     mentor_email VARCHAR,
-    mentor_facebook VARCHAR,
-    mentor_instagram VARCHAR,
     mentor_phone VARCHAR,
     mentor_colleges VARCHAR,
     mentor_major1 VARCHAR,
