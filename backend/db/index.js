63812549dd86b76ce3402428bd73372f51032529
oliviasl <oslai@usc.edu>
2024-06-09 22:06:49 -0700

    Remove sequelize

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const { Pool } = require("pg");
-const pg = require('pg');
 
-const pool = new pg.Pool();
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const db = require('./db/index.js');
 require("dotenv").config();
 
 const collegeController = require("./controller/college.controller");
 const mentorController = require("./controller/mentor.controller");
 const userController = require("./controller/user.controller");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -612,6 +612,6 @@ app.post("/api/paginatedMentorsFiltered", (req, res) => {
 });
 
 // Start Backend Port
-app.listen(port, () => {
-  console.log(`Server listening on the port  ${port}`);
+app.listen(3000, () => {
+  console.log(`Server listening on the port  3000`);
 });
